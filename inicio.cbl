6bcef505010c00a6042d459b070a950424daa6f0
camilonfs1 <camilonfs1@gmail.com>
2021-01-09 14:25:04 -0500

    segundo commit

--- inicio.cbl
+++ inicio.cbl
@@ -6,13 +6,58 @@
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. INICIO.
+
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+
+           SELECT OPTIONAL CLIENTES ASSIGN TO "./clientes.dat"
+                  ORGANIZATION INDEXED
+                  ACCESS MODE DYNAMIC
+                  RECORD KEY IS ID-CLIENTE
+                  ALTERNATE KEY CLI_NOMBRE WITH DUPLICATES
+                  ALTERNATE KEY CLI_ALT_2  WITH DUPLICATES
+                  STATUS ST-FILE.
+
        DATA DIVISION.
        FILE SECTION.
+
+       FD  CLIENTES.
+
+       01  REG-CLIENTES.
+           03  ID-CLIENTE.
+               05 CLI-ID           PIC 9(8).
+           03  CLI-SALDO           PIC S9(7)V9(3).
+           03  CLI_NOMBRE          PIC X(60).
+           03  CLI_DIRECCION       PIC X(80).
+           03  CLI_CODPOST         PIC X(80).
+           03  CLI_CATEGORIA       PIC X.
+           03  CLI_ALT_2.
+               05 CLI_CATEGORIA_2  PIC X.
+               05 CLI_NOMBRE_2     PIC X(80).
+
+
+
        WORKING-STORAGE SECTION.
+
+       01  ST-FILE PIC XX.
+
        PROCEDURE DIVISION.
        PEIMER-RUTINA.
-            DISPLAY "Hello world".
-       SEGUNDA-RUTINA.
-           DISPLAY "Hello world 2"
+           PERFORM ABRO-ARCHIVO.
+           PERFORM CIERRE-ARCHIVO.
            STOP RUN.
+
+       ABRO-ARCHIVO.
+           DISPLAY "Abriendo archivo".
+           OPEN I-O CLIENTES.
+           IF ST-FILE > "07"
+               DISPLAY "Error abriendo el archivo".
+
+       CIERRE-ARCHIVO.
+           CLOSE CLIENTES.
+
        END PROGRAM INICIO.
