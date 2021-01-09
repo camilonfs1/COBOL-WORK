cdc58d08ffb99fcad6cd795759e56f29c5ca0b00
camilonfs1 <camilonfs1@gmail.com>
2021-01-09 10:07:21 -0500

    first commit

---
+++ inicio.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author: Camilo Ernesto Vargas Romero
+      * Date: 09 - 01 - 2021
+      * Purpose: Practice
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. INICIO.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM INICIO.
